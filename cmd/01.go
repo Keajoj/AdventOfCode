ee8c1f844ca4491f77d458a7d78d80125377b0e3
Keajoj <joshua.thomas1999@gmail.com>
2023-01-22 01:04:14 -0700

    Implements Day 1 Part 1

--- cmd/01.go
+++ cmd/01.go
@@ -5,26 +5,32 @@ import "bufio"
 import "strconv"
 import "os"
 
-func dayOnePart1 (inputFile string) {
-	fmt.Println(inputFile)
-		fmt.Println(InputFilepath)
-		file, err := os.Open(InputFilepath)
-		if err != nil {
-			os.Exit(1)
-		}
-		defer file.Close()
+func dayOnePart1 () {
+	file, err := os.Open(InputFilepath)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer file.Close()
 
-		scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-		for scanner.Scan() {
-			//fmt.Println(scanner.Text())
-			value, err := strconv.Atoi(scanner.Text())
-			if err != nil { os.Exit(1) }
-			fmt.Printf("Value as int: %d\n", value)
-			
+	var curr int = 0
+	var max int = 0
+	for scanner.Scan() {
+		value, err := strconv.Atoi(scanner.Text())
+		if err == nil {
+			curr += value
+		} else {
+			if curr > max { 
+				max = curr
+			}
+			curr = 0
 		}
+	}
 
-		if err := scanner.Err(); err != nil {
-			os.Exit(1)
-		}
-    }
+	fmt.Printf("Max value: %d",max);
+
+	if err := scanner.Err(); err != nil {
+		os.Exit(1)
+	}
+}
